50d89fcd723a206974c3e14fddb07c9916fe5a53
Its_Nongamba <121596364+Nongamba04@users.noreply.github.com>
2023-09-09 00:42:41 +0530

    Create script.js

---
+++ 5/script.js
@@ -0,0 +1,14 @@
+// Installing Express + Postman
+
+//  Postman is an API(application programming interface) development tool which helps to build, test and modify APIs.
+// Try runing more post api request for better understanding
+
+// Express is a small and open-source framework that sits on top of Node.js’s web server functionality to simplify
+//  its APIs and add helpful new features.It makes it easier to organize your application’s 
+//  functionality with middle ware and routing; it adds helpful utilities to Node.js’s HTTP objects;
+//  it facilitates the rendering of dynamic HTTP objects.
+// It makes url parsing easy(Refer to the '2' folder)
+// handles routing
+
+// To install Express, first write 'npm init' in cmd 
+// Then write npm install express
