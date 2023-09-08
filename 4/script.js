42c7e529a3a7ef2bc6a26a8c80f108b16f03de44
Its_Nongamba <121596364+Nongamba04@users.noreply.github.com>
2023-09-09 00:40:31 +0530

    Create script.js

---
+++ 4/script.js
@@ -0,0 +1,10 @@
+// npm : Node Package Manager
+// node_modules :stores every module an user requires for its project
+// npm install - command will install the node modules folder according to the dependencies mentioned inside the package.json
+// example : npm install slugify@1.3.5,to install new packages inside the dependencies (dependent packages )
+
+// npm install nodemon --save-dev : devDepencies(Only uses in development and not in production)
+
+// For global installation : npm install (Package name) --global
+
+// npm i is also generally used as a install command
